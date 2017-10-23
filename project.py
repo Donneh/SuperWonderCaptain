26d6e19bb31e66f752903538354e6e130949462f
Youri van Maanen <youri@skick.eu>
2017-10-23 21:10:26 +0200

    First push

---
+++ project.py
@@ -0,0 +1,5 @@
+name=str(input("Welke superheld denk je dat het is?"))
+while name !="random superheld":
+    print ("Dit is niet correct")
+    name = str(input("Welke superheld denk je dat het is?"))
+    print("Dit antwoord is correct")
