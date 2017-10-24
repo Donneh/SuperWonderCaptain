f751daef4edee7500968ee335e1d587a016b44b0
Donny van Walsem <donnehvw@gmail.com>
2017-10-24 10:26:49 +0200

    Add first attempt at scoreboard

---
+++ main.py
@@ -0,0 +1,50 @@
+import datetime
+from tkinter import *
+from tkinter import ttk
+
+def inputHighscore():
+    name = input("What is your name? ")
+    score = input("What is your score? ")
+    time = str(datetime.datetime.now())
+    string = ','.join([name, score, time])
+    file = open("highscore.txt", "a")
+    file.write(string + '\n')
+    file.close()
+
+
+def getHighscores():
+    dict = {}
+    with open('highscore.txt', 'r') as file:
+        index = 0
+        for line in file:
+            parts = line.split(',')
+            dict[index] = {}
+            dict[index]['name'] = parts[0]
+            dict[index]['score'] = parts[1]
+            dict[index]['datetime'] = parts[2]
+            index += 1
+
+    return dict
+
+
+
+root = Tk()
+frame = Frame(root, width=200, height=100)
+tree = ttk.Treeview(frame)
+
+tree['columns'] = (0,1,2)
+tree['show'] = 'headings'
+tree.column(0, width=100)
+tree.heading(0, text="Name")
+tree.column(1, width=100)
+tree.heading(1, text="Score")
+tree.column(2, width=100)
+tree.heading(2, text="Datetime")
+
+for key, user in getHighscores().items():
+    tree.insert("", 0, user['name'], values=(user['name'], user['score'], user['datetime']))
+
+tree.grid(column=3, row=1)
+tree.pack(expand=YES)
+frame.pack()
+mainloop()
